5593dfa191b9235fc920be6ea5eb93e817464192
RichardvanLeeuwen <richard@leeuwenmail.com>
2023-08-11 02:22:58 +0200

    Visual edits of colors and texts

--- GUI.java
+++ GUI.java
@@ -4,14 +4,26 @@ import java.awt.Font;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
 
 public class GUI {
     private JFrame frame = new JFrame();
     private JPanel panel = new JPanel();
+    private JPanel infoPanel = new JPanel();
+
+    private String getLanguageInformationString() {
+        String currentStreak = "Streak: 0\n";
+        String dailyTasksCompleted = "Daily tasks: 0/3\n";
+        String timeTillNextTasks = "Time remaining: 18:00\n";
+        String infoCombined = new StringBuilder().append(currentStreak).append(dailyTasksCompleted)
+                .append(timeTillNextTasks).toString(); // consider a loop for readability
+        return infoCombined;
+    }
 
     public GUI() {
 
@@ -23,12 +35,26 @@ public class GUI {
         LanguageButton ankiButton = new LanguageButton("Anki");
         LanguageButton audioLessonsButton = new LanguageButton("Audio lessons");
 
-        JLabel label = new JLabel("Test language information");
-        label.setFont(new Font("Helvitca", Font.PLAIN, 40));
-        label.setHorizontalAlignment(SwingConstants.CENTER);
+        infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS));
+        infoPanel.setBorder(BorderFactory.createEmptyBorder(5, 10, 10, 10));
+        infoPanel.setBackground(new Color(255, 218, 185));
+
+        JLabel languageInfoLabel = new JLabel("Test language information");
+        languageInfoLabel.setFont(new Font("Helvitca", Font.BOLD, 40));
+        languageInfoLabel.setForeground(new Color(65, 105, 225));
+        languageInfoLabel.setHorizontalAlignment(SwingConstants.LEFT);
+        languageInfoLabel.setAlignmentX(JLabel.LEFT_ALIGNMENT);
+
+        JTextArea languageInfo = new JTextArea(getLanguageInformationString(), 3, 0);
+        languageInfo.setFont(new Font("Helvitca", Font.PLAIN, 25));
+        languageInfo.setEditable(false);
+        languageInfo.setAlignmentX(JTextArea.LEFT_ALIGNMENT);
+
+        infoPanel.add(languageInfoLabel);
+        infoPanel.add(languageInfo);
 
         panel.add(duolingoButton);
-        panel.add(label);
+        panel.add(infoPanel);
         panel.add(ankiButton);
         panel.add(audioLessonsButton);
 
--- LanguageButton.java
+++ LanguageButton.java
@@ -11,12 +11,17 @@ public class LanguageButton extends JButton implements ActionListener {
         this.setBackground(new Color(37, 245, 78));
     }
 
+    private void turnForegroundWhite() {
+        this.setForeground(Color.WHITE);
+    }
+
     public LanguageButton(String name, String iconLocation) {
         super(name, new ImageIcon(iconLocation));
         this.setVerticalTextPosition(SwingConstants.BOTTOM);
         this.setHorizontalTextPosition(SwingConstants.CENTER);
         this.setFont(new Font("Helvitca", Font.PLAIN, 40));
-        this.setBackground(Color.lightGray);
+        this.setBackground(new Color(192, 192, 192));
+        this.setForeground(new Color(25, 81, 144));
         this.addActionListener(this);
 
     }
@@ -24,11 +29,13 @@ public class LanguageButton extends JButton implements ActionListener {
     public LanguageButton(String name) {
         super(name);
         this.setFont(new Font("Helvitca", Font.PLAIN, 40));
-        this.setBackground(Color.lightGray);
+        this.setBackground(new Color(192, 192, 192));
+        this.setForeground(new Color(25, 81, 144));
         this.addActionListener(this);
     }
 
     public void actionPerformed(ActionEvent e) {
         turnBackgroundGreen();
+        turnForegroundWhite();
     }
 }
